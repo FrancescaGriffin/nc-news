c5fca50ffc7d4585426fa3620d9c1a5747fdd9d6
Francesca Kissack <francesca.kissack@gmail.com>
2021-12-01 21:56:55 +0000

    updated title, hosted app with Netlify

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="header">
-            <h1> First News </h1>
+            <h1> The Everyday Journalist </h1>
         </header>
     )
 }
